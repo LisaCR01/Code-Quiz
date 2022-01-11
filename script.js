736400fcf8643da9bae17dc8669aa0614e0a7c99
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-11 15:27:35 +0000

    timer enabled when start button clicked

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-//When the start_btn is clicked timer starts at 75seconds
-//When the start_btn is clicked gives you the first question
-//For every wrong answer selected subtract 5 seconds from time
-//When finished questions timer goes to 0
-//When finished can type in name and places your score next to it for leadership board
-//computer needs to know which answer is the correct one
+//when button clicked it disappears and timer starts
+//timer displayed instead of good luck
+var timeEl = document.querySelector(".timer");
+// Selects element by id
+var secondsLeft = 75;
+function setTime() {secondsLeft = 75;
+   // Sets interval in variable
+   var timerInterval = setInterval(function() {
+     secondsLeft--
+     if(secondsLeft <= 0 ) {
+       // Stops execution of action at set interval
+       clearInterval(timerInterval);
+       // Calls function to do leadership board
+     } 
+     //need the max in here -in case my score goes negative. 
+     else{timeEl.textContent = Math.max(secondsLeft,0) + " seconds left of quiz"};
+   }, 1000);
+   }
+var button = document.querySelector("#start_btn");
 
-/*
-5 questions that can be asked
-- which is the correct opening for a Single Line Comment in JavaScript.
-    -"//"
-    -"<!--"
-    -"- -"
-    -"#"
-- what do arrays store
-    -"strings"
-    -""
-- 
--
--
+button.addEventListener("click", startTimer)
+var istep=-1
+function startTimer(event) {
+if(istep==-1){    // event.stopPropagation();
+    
+setTime();
+ ;}
 
-*/
+}
