b73f2c5bb3ea7c14b51d1284a74a5d8d5d693638
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-11 14:46:58 +0000

    updated css to improve appearance of initial page when user loads website

---
+++ script.js
@@ -0,0 +1,22 @@
+//When the start_btn is clicked timer starts at 75seconds
+//When the start_btn is clicked gives you the first question
+//For every wrong answer selected subtract 5 seconds from time
+//When finished questions timer goes to 0
+//When finished can type in name and places your score next to it for leadership board
+//computer needs to know which answer is the correct one
+
+/*
+5 questions that can be asked
+- which is the correct opening for a Single Line Comment in JavaScript.
+    -"//"
+    -"<!--"
+    -"- -"
+    -"#"
+- what do arrays store
+    -"strings"
+    -""
+- 
+-
+-
+
+*/
