85e6a08dde31cdd8202fa0d04d820fc777211587
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2022-01-14 19:00:45 +0000

    updated my readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Code-Quiz
+In this project I created a timed quiz on JavaScript fundamentals that stores high scores.
+The purposes of this was
+
+## Criteria that I met to achieve this
+
+*When the user clicks the start button a timer starts and they are presented with the a question.
+*When the user answers a question they are presented with another question.
+*When the user answers it incorrectly then time is subtracted from the clock.
+*When the user has answered all of the questions the timer reaches 0 and the game is over.
+*When the user has finished the game they can save their initials and score on the leadership board.
+
+## Deployed Application
--- script.js
+++ script.js
@@ -64,6 +64,7 @@ function setTime() {secondsLeft = 75;
        // Calls function to do leadership board
      
      timeEl.textContent = "Your score is "+ Math.max(secondsLeft,0);
+     document.querySelector(".rules_card").innerHTML=codeBlockend 
     }
      //need the max in here -in case my score goes negative. 
      else{timeEl.textContent = Math.max(secondsLeft,0) + " seconds left of quiz"};
@@ -100,8 +101,9 @@ function genQ(){
     document.getElementById("dchoice").innerHTML=questionBank[istep].ans.d}
     }
 
-    var failPic='<p>&#x2718; </p>'
+   var failPic='<p>&#x2718; </p>'
     var rightPic='<p>&#10004;</p>'
+    
   
     const selVal=[]
     function getAns(){
@@ -115,6 +117,7 @@ function genQ(){
       {document.getElementById("try").style.color = "rgb(234, 200, 231)";document.getElementById("try").innerHTML = rightPic}
     genQ()
     }
+
     
     var codeBlock = 
   '<section class="content">' +
@@ -129,5 +132,103 @@ function genQ(){
     '</select>' +
     '</section>'
   ;
+  
+//the leaderboard
+
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = [];
+
+// The following function renders items in a todo list as <li> elements
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    //changed this line here
+    li.textContent = todo.initialS +" High Score "+todo.score;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Delete";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
+}
+
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // This is a helper function that will render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  //sort first
+  todos.sort(function(a, b){return b.score - a.score});
+  console.log(todos)
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Add submit event to form
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  var elementNew={initialS:todoText,score:secondsLeft}
+  // Add new todoText to todos array, clear the input
+  todos.push(elementNew)
+  todoInput.value = "";
+  
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+
+  setInterval(function(){ document.location.reload(); }, 3000);
+});
+
+// Add click event to todoList element
+todoList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+  }
+});
+
+// Calls init to retrieve data and render it to the page on load
+init()
 
     
